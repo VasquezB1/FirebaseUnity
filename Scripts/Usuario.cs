faffa520515c0ff4869d8cd4fb7f75bd84c1bb18
VásquezByron1 <bvasquezs@est.ups.edu.ec>
2022-06-05 01:19:48 -0500

    Creacion Clases Usuario

---
+++ Scripts/Usuario.cs
@@ -0,0 +1,23 @@
+﻿//Declaramos la Clase Usuario la cual sera una de nuestras primeras clases
+
+public class Usuario
+{
+    //Parametros que usaremos
+    public string nombre;
+    public string apellido;
+    public string telefono;
+    public string email;
+
+    //Constructor de nuestra clase
+    public Usuario()
+    {
+    }
+
+    public Usuario(string nombre, string apellido, string telefono, string email)
+    {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.email = email;
+    }
+}
