e6dffe206f8072eb110d4a21889a8640ce21e16c
VásquezByron1 <bvasquezs@est.ups.edu.ec>
2022-06-07 21:02:32 -0500

    Eliminar Lector

---
+++ Assets/Scripts/EliminarLector.cs
@@ -0,0 +1,147 @@
+﻿using Firebase.Database;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EliminarLector : MonoBehaviour
+{
+
+    public InputField userID; //Valor de cedula
+
+    public Text nombrePersona;
+    public Text apellidoPersona;
+    public Text telefonoPersona;
+    public Text emailPersona;
+
+    DatabaseReference mDatabaseRef;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        mDatabaseRef = FirebaseDatabase.DefaultInstance.RootReference;
+        ObtenerDatos();
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void ObtenerDatos()
+    {
+        ListarUsuarios();
+    }
+
+    public void eliminarDatos()
+    {
+        var usuarioID = mDatabaseRef.Child("Usuarios").Child(userID.text).Child("userID").GetValueAsync();
+        // yield return new WaitUntil(predicate: () => usuarioID.IsCompleted);
+        Usuario user = new Usuario(userID.text, " ", " ", " ", " ");
+        string json = JsonUtility.ToJson(user);
+        mDatabaseRef.Child("Usuarios").Child(userID.text).SetRawJsonValueAsync(json);
+    }
+
+
+    public IEnumerator GetNombre(Action<string> onCallBack)
+    {
+        var userNombre = mDatabaseRef.Child("Usuarios").Child(userID.text).Child("nombre").GetValueAsync();
+
+        yield return new WaitUntil(predicate: () => userNombre.IsCompleted);
+
+
+        if (userNombre != null)
+        {
+
+            DataSnapshot datos = userNombre.Result;
+            onCallBack.Invoke(datos.Value.ToString());
+        }
+
+    }
+
+    public IEnumerator GetApellido(Action<string> onCallBack)
+    {
+        var userApellido = mDatabaseRef.Child("Usuarios").Child(userID.text).Child("apellido").GetValueAsync();
+
+        yield return new WaitUntil(predicate: () => userApellido.IsCompleted);
+
+
+        if (userApellido != null)
+        {
+
+            DataSnapshot datos = userApellido.Result;
+            onCallBack.Invoke(datos.Value.ToString());
+        }
+
+    }
+
+    public IEnumerator GetTelefono(Action<string> onCallBack)
+    {
+        var userTelefono = mDatabaseRef.Child("Usuarios").Child(userID.text).Child("telefono").GetValueAsync();
+
+        yield return new WaitUntil(predicate: () => userTelefono.IsCompleted);
+
+
+        if (userTelefono != null)
+        {
+
+            DataSnapshot datos = userTelefono.Result;
+            onCallBack.Invoke(datos.Value.ToString());
+        }
+
+    }
+
+    public IEnumerator GetEmail(Action<string> onCallBack)
+    {
+        var userEmail = mDatabaseRef.Child("Usuarios").Child(userID.text).Child("email").GetValueAsync();
+
+        yield return new WaitUntil(predicate: () => userEmail.IsCompleted);
+
+
+        if (userEmail != null)
+        {
+            DataSnapshot datos = userEmail.Result;
+            onCallBack.Invoke(datos.Value.ToString());
+        }
+
+    }
+
+
+
+    public void ListarUsuarios()
+    {
+        StartCoroutine(GetNombre((string nombre) =>
+        {
+            nombrePersona.ToString();
+            nombrePersona.text = nombre;
+        }));
+
+        StartCoroutine(GetApellido((string apellido) =>
+        {
+            apellidoPersona.ToString();
+            apellidoPersona.text = apellido;
+        }));
+
+        StartCoroutine(GetTelefono((string telefono) =>
+        {
+            telefonoPersona.ToString();
+            telefonoPersona.text = telefono;
+        }));
+
+        StartCoroutine(GetEmail((string email) =>
+        {
+            emailPersona.ToString();
+            emailPersona.text = email;
+        }));
+
+    }
+
+}
